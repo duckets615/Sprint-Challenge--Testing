324de64a385c5909de32fb2bb1dbacc711bcc814
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-04-12 13:17:21 -0400

    post game ep test

--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,7 @@
 const server = require('./api/server');
 const request = require('supertest');
 
-// GET All Games EP
+// get all games EP
 describe('GET /games Endpoint', () => {
     beforeAll( async () => {
         return response = await request(server).get('/games');
@@ -42,7 +42,7 @@ describe('GET /games Endpoint', () => {
 
 });
 
-// Get Indiv EP test
+// get iIndiv EP test
 describe('GET /games/id', () => {
     beforeAll( async () => {
         return res = {
@@ -78,4 +78,104 @@ describe('GET /games/id', () => {
         });
     });
 
+});
+
+// post game ep
+describe('POST /games Endpoint', () => {
+    beforeAll( async () => {
+        return res = await request(server).post('/games').send({
+            title: 'Joust',
+            genre: 'Arcade',
+            releaseYear: 1982
+        });
+    });
+
+
+    it('Should respond with JSON', () => {
+        expect(res.type).toBe('application/json');
+    });
+
+
+// test fail
+    describe('Endpoint Failure Tests', () => {
+        // Fail res's
+        beforeAll( async () => {
+            // valid request for dupe test
+            await request(server).post('/games').send({ title: 'Centipede', genre: 'Arcade', releaseYear: 1980 });
+
+            // object with fail res's
+            return failureResponses = {
+                missingTitle: await request(server).post('/games').send({ title: 'Centipede', releaseYear: 1980 }),
+                missingGenre: await request(server).post('/games').send({ genre: 'Arcade', releaseYear: 1980 }),
+                missingBoth: await request(server).post('/games').send({ releaseYear: 1980 })
+            }
+        });
+
+        it ('Should respond with a status code of 422 (Uprocessible Entity) if not sent a title or genre', () => {
+            const errorCode = 422;
+
+            expect(failureResponses.missingTitle.status).toBe(errorCode);
+            expect(failureResponses.missingGenre.status).toBe(errorCode);
+            expect(failureResponses.missingBoth.status).toBe(errorCode);
+        })
+
+        it ('Should respond with an errorMessage if not sent a title or genre', () => {
+            const errorMessage = { errorMessage: "You must provide a title and genre when adding a game." };
+
+            expect(failureResponses.missingTitle.body).toEqual(errorMessage);
+            expect(failureResponses.missingGenre.body).toEqual(errorMessage);
+            expect(failureResponses.missingBoth.body).toEqual(errorMessage);
+        })
+
+        it('Should respond with a status code of 405 (Not Allowed) if sent a duplicate game title', async () => {
+            const errorCode = 405;
+            const duplicateTitle = await request(server).post('/games').send({ title: 'Centipede', genre: 'Mobile', releaseYear: 2015 });
+            expect(duplicateTitle.status).toBe(errorCode);
+        })
+
+        it('Should respond with an errorMessage if sent a duplicate game title', async () => {
+            const errorMessage = { errorMessage: "A game with that title was already added. Titles must be unique." };
+            const duplicateTitle = await request(server).post('/games').send({ title: 'Centipede', genre: 'Mobile', releaseYear: 2015 });
+            expect(duplicateTitle.body).toEqual(errorMessage);
+        })
+
+    })
+
+// PASSED TEST
+    describe('Endpoint Success Tests', () => {
+        beforeAll( async () => {
+            return missingReleaseYear = await request(server).post('/games').send({ title: 'Galaga', genre: 'Arcade' });
+        });
+
+        it('Should respond with a status code of 201 (Created) after a successful post', () => {
+            expect(res.status).toBe(201);
+
+            // release year not req so still pass
+            expect(missingReleaseYear.status).toBe(201);
+        });
+
+        it('Should respond with the unit after the unit is added.', () => {
+            expect({ title: res.body.title, genre: res.body.genre, releaseYear: res.body.releaseYear }).toEqual({
+                title: 'Joust',
+                genre: 'Arcade',
+                releaseYear: 1982
+            });
+
+            // release year not req so still pass
+            expect({ title: missingReleaseYear.body.title, genre: missingReleaseYear.body.genre, releaseYear: missingReleaseYear.body.releaseYear }).toEqual({
+                title: 'Galaga',
+                genre: 'Arcade',
+                releaseYear: ''
+            });
+        });
+
+        it('Should have increased the size of the unit resource by 1', async () => {
+            const initialGetResponse = await request(server).get('/games');
+            await request(server).post('/games').send({ title: 'Frogger', genre: 'Arcade', releaseYear: 1981 });
+            const finalGetResponse = await request(server).get('/games');
+
+            expect(finalGetResponse.body.length).toBe(initialGetResponse.body.length + 1);
+        });
+
+    });
 });
